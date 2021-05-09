eeb5dcc9434f607460fb299f508b93be631fd4f2
AnuraghSarkar <anuraghtimalsina@gmail.com>
2021-05-09 11:03:01 +0545

    Filtered few days ago post

--- exercise1/main.py
+++ exercise1/main.py
@@ -5,5 +5,16 @@ import requests
 html_text = requests.get(
     'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text, 'lxml')
-job = soup.find('li', class_='clearfix job-bx wht-shd-bx')
-company_name = soup.find('h3', class_='joblist-comp-name')
+jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+
+for job in jobs:
+    published_date = job.find('span', class_='sim-posted').span.text
+    if 'few' in published_date: 
+        company_name = job.find('h3', class_='joblist-comp-name').text.strip()
+        skills = job.find('span', class_='srp-skills').text.strip().replace(' ','')
+        print(f'''
+        Company Name : {company_name}
+        Required Skills : {skills}
+        Published Date: {published_date}
+        ''')
+        print('-------------------------------------------------------')
