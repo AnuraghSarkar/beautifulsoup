6023617726422751f38955b334871c9b718fb697
AnuraghSarkar <anuraghtimalsina@gmail.com>
2021-05-09 10:30:01 +0545

    Loop while selecting

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from bs4 import BeautifulSoup
-with open('index.html','r') as html_file:
+with open('index.html', 'r') as html_file:
     content = html_file.read()
     soup = BeautifulSoup(content, 'lxml')
-    
-    tags = soup.find('h5')
-    print(tags)
+
+    # courses_html_tags = soup.find_all('h5')
+    # for course in courses_html_tags:
+    #     print(course.text)
+
+    course_carts = soup.find_all('div', class_='card')
+    for courses in course_carts:
+        course_name = courses.h5.text
+        course_price = courses.a.text
+        print(f'{course_name} for {course_price}')
